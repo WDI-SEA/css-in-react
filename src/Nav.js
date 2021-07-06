bcff9e16d4d1de987bf3f16ae435f9f9d77d902d
Anna Zocher <anna.zocher@gmail.com>
2021-07-06 13:32:33 -0700

    example of react inline styling in Nav

---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import './Nav.css'
+
+export default function Nav() {
+    return (
+        <div style={{backgroundColor: 'lightpink'}}>
+            <nav>
+                <h1>Hello!</h1>
+            </nav>
+        </div>
+    )
+}

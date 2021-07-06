1e8c388e09d2f8932b1940109abd715fb25db697
Anna Zocher <anna.zocher@gmail.com>
2021-07-06 14:02:42 -0700

    added styled components example

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,48 @@
+import styled, { css, keyframes }  from 'styled-components'
 import './Nav.css'
+
+const fade = keyframes`
+    0% {
+       opacity: 0;
+    }
+
+    25% {
+        opacity: .5;
+    }
+
+    50% {
+        opacity: .65;
+    }
+
+    100% {
+        opacity: 1;
+    }
+`
+
+const NavHolder = styled.nav`
+    border: 2px solid white;
+    color: white;
+    text-align: center;
+    animation: ${fade} 5s linear infinite;
+`
+
+const Header = styled.h1`
+    color: lightblue;
+    font-size: 35px;
+`
+
+const PinkHeader = styled(Header)`
+    color: lightpink;
+`
+
 
 export default function Nav() {
     return (
-        <div style={{backgroundColor: 'lightpink'}}>
-            <nav>
-                <h1>Hello!</h1>
-            </nav>
+        <div>
+            <NavHolder>
+                <Header>Hello!</Header>
+                <PinkHeader>Hello, again!</PinkHeader>
+            </NavHolder>
         </div>
     )
 }
